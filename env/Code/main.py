da8b4cd49b922bde27d712d12df1246ea5133d97
Ganesh Jainarain <RicheyJay2@gmail.com>
2023-03-08 15:58:27 -0500

    started our ipynb file

--- env/Code/main.py
+++ env/Code/main.py
@@ -31,4 +31,23 @@ import warnings
 warnings.filterwarnings('ignore')
 print('-'*25)
 
+pd.set_option('display.max_columns', None)
 
+data_raw = pd.read_csv('/Users/richeyjay/Desktop/BinaryClass_PulsarData_Kaggle/env/Data/train.csv')
+
+#a dataset should be broken into 3 splits: train, test, and (final) validation
+#the test file provided is the validation file for competition submission
+#we will split the train set into train and test data in future sections
+data_val  = pd.read_csv('/Users/richeyjay/Desktop/BinaryClass_PulsarData_Kaggle/env/Data/test.csv')
+
+#to play with our data we'll create a copy
+#remember python assignment or equal passes by reference vs values, so we use the copy function: https://stackoverflow.com/questions/46327494/python-pandas-dataframe-copydeep-false-vs-copydeep-true-vs
+data1 = data_raw.copy(deep = True)
+
+#however passing by reference is convenient, because we can clean both datasets at once
+data_cleaner = [data1, data_val]
+
+#preview data
+print(data_raw.info())
+
+print(data_raw.sample(10))
